2a05fbfc674ec22ce7d86bea8656cd6a52ac2c81
Victor <victorh.pedr@gmail.com>
2024-10-01 18:29:58 -0300

    chore: back to version 14 next

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
-import "src/presentation/styles/globals.css";
 import type { AppProps } from "next/app";
+
+import "src/presentation/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
